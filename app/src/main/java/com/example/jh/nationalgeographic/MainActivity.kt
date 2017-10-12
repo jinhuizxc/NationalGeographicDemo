cae0e5b41061ffa2c14b31dc73a8c80c7487cf14
JinHui <1004260403@qq.com>
2017-10-12 14:19:37 +0800

    用kotlin编写

---
+++ app/src/main/java/com/example/jh/nationalgeographic/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.jh.nationalgeographic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * 学习前辈的国家地理项目，用kotlin编写！
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
